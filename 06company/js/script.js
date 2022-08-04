648bf56eb4a83cc5b74d27d01e707aaa367a7f21
alsrl1234 <alsrlrk123@naver.com>
2022-08-04 18:01:07 +0900

    1

--- 06company/js/script.js
+++ 06company/js/script.js
@@ -24,6 +24,7 @@ $(".navbar01>ul>li").hover(function(){
 $(".toggle").click(function(){
     $(".navbarClone").css("left",0);
     $(".overlay").show();
+    
 })
 $(".overlay").click(function(){
     $(".navbarClone").css("left","-100%");
@@ -39,4 +40,22 @@ $(".contents2 li").click(function(){
     $(".contents2 li").removeClass("click")
     $(this).addClass("click")
     
+})
+let i = 0;
+$(".mMenu>ul>li").click(function(){
+    event.preventDefault();
+    if(i == 0){
+        $(".submenu").hide()
+        $(this).find(".submenu").show();
+        $(this).children("a").css("color","#027ac4")
+        $(this).children("a").addClass("on")
+        i = 1;
+      console.log(i)
+      
+    }else {
+        $(this).find(".submenu").hide();
+        $(this).children("a").css("color","#333")
+        $(this).children("a").removeClass("on")
+        i = 0;
+    }
 })
