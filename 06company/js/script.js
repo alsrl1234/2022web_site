f552301aa413519362f88e82a48ed077b856addf
alsrl1234 <alsrlrk123@naver.com>
2022-08-05 18:01:08 +0900

    1

--- 06company/js/script.js
+++ 06company/js/script.js
@@ -1,61 +1,60 @@
 const slide = $(".slideWrap").bxSlider({
     controls: false,
-    
+
 });
 
-$(".next").click(function(){
+$(".next").click(function () {
     slide.goToNextSlide();
 });
 
-$(".prev").click(function(){
+$(".prev").click(function () {
     slide.goToPrevSlide();
 });
-    
 
 
 
 
-$(".navbar01>ul>li").hover(function(){
+
+$(".navbar01>ul>li").hover(function () {
     $(this).addClass("on");
-},function(){
+}, function () {
     $(this).removeClass("on");
 });
 
-$(".toggle").click(function(){
-    $(".navbarClone").css("left",0);
+$(".toggle").click(function () {
+    $(".navbarClone").css("left", 0);
     $(".overlay").show();
-    
+    $(".mMenu>ul>li>a").removeClass("on")
+    $(".submenu").hide()
+
 })
-$(".overlay").click(function(){
-    $(".navbarClone").css("left","-100%");
+$(".overlay").click(function () {
+    $(".navbarClone").css("left", "-100%");
     $(".overlay").hide();
 })
-$(".mClose").click(function(){
-    $(".navbarClone").css("left","-100%");
+$(".mClose").click(function () {
+    $(".navbarClone").css("left", "-100%");
     $(".overlay").hide();
 });
 // $(".navbarWrap").clone().appendTo(".mMenu");
 $(".navbar01>ul").clone().appendTo(".mMenu");
-$(".contents2 li").click(function(){
+$(".contents2 li").click(function () {
     $(".contents2 li").removeClass("click")
     $(this).addClass("click")
-    
+
 })
-let i = 0;
-$(".mMenu>ul>li").click(function(){
-    event.preventDefault();
-    if(i == 0){
+
+
+
+$(".mMenu>ul>li>a").click(function (e) {
+    e.preventDefault();
+    if ($(this).hasClass("on")) {
+        $(this).removeClass("on")
         $(".submenu").hide()
-        $(this).find(".submenu").show();
-        $(this).children("a").css("color","#027ac4")
-        $(this).children("a").addClass("on")
-        i = 1;
-      console.log(i)
-      
-    }else {
-        $(this).find(".submenu").hide();
-        $(this).children("a").css("color","#333")
-        $(this).children("a").removeClass("on")
-        i = 0;
+    } else {
+        $(".mMenu>ul>li>a").removeClass("on")
+        $(".submenu").hide()
+        $(this).parent().find(".submenu").show()
+        $(this).addClass("on")
     }
 })
